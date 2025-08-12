541727cdf22c10472f39848fdc67957578ea351a
Sardar0715 <guseynovsardar715@gmail.com>
2025-08-12 14:05:05 +0500

    Create script.py

---
+++ lesson-2/Homework/script.py
@@ -0,0 +1,30 @@
+txt = 'MsaatmiazD'
+txt[::2]
+msg="I'm John. I am from London"
+msg.split()[-1]
+
+laptop= input("matnni kiriting")
+laptop[::-1]
+txt=["olama","anor","tarvuz","nok","anjir"]
+txt[2]
+number1=[1,2,3,4,5]
+number2=[6,7,8,9,10]
+
+number1.extend(number2)
+print(number1)
+count=[24,54,23,67,86,100]
+
+first=count[0]
+midle=count[len(count)//2]
+last=count[-1]
+new_list=[first,midle,last]
+print(new_list)
+graf=[1,2,45,65,21]
+graf=graf*2
+print(graf)
+
+look=[122,224,552,12,2551]
+look[0],look[-1]=look[-1],look[0]
+print(look)
+
+
